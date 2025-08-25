ca275ded93c9b13220bb90be331ad00d77f7602e
DiscoDoggy <akaattarilim@gmail.com>
2025-08-25 10:06:12 -0700

    [MODIFYING EXISTING BEHAVIOR] Implemented ability to handle schools having multiple email domains * Existing behavior is that script only handles ingesting a single school domain by just taking the first domain in the list * To solve the problem of some universities having multiple email domains, we created an additional db table in supabase for school domains. We then create UUIDs in the code for schools, bulk insert the schools, then using the IDs we created, bulk insert all domains for each school into the school_email_domain table.

--- app/scripts/school_ingestion_script/schools_table_ingestion.py
+++ app/scripts/school_ingestion_script/schools_table_ingestion.py
@@ -3,6 +3,7 @@ import os
 import supabase
 from supabase import create_client, Client
 import json
+import uuid
 
 class DBHandler:
     def __init__(self):
@@ -15,15 +16,16 @@ class DBHandler:
         self.supabase_client: Client = create_client(supabase_url, supabase_private_key)
 
 class School:
-    def __init__(self, name: str, country_code: str, domain: str):
+    def __init__(self, name: str, country_code: str, domains: list[str]):
         self.name = name
         self.country_code = country_code
-        self.domain = domain
+        self.domains = domains
+
     def get_dict(self):
         return {
             "name": self.name,
             "country_code": self.country_code,
-            "email_domain": self.domain
+            "email_domain": self.domains
         }
 
 class SchoolsIngestor:
@@ -40,18 +42,45 @@ class SchoolsIngestor:
             school: School = School(
                 name=school_info["name"],
                 country_code=school_info["alpha_two_code"],
-                domain=school_info["domains"][0] #returns a list but so far haven't seen any schools with more than one domain...
+                domains=school_info["domains"]
             )
 
             schools.append(school)
         return schools
 
     def ingest_schools_into_supabase(self, schools: list[School]):
+        school_id_to_domains : dict[str, str] = {}
         schools_as_dict = []
-        for school in schools:
-            schools_as_dict.append(school.get_dict())
+        # we explicitly generate a random uuid s.t we have a visible link in the code between school
+        # and school domain. This is needed because we are bulk inserting for performance. We cannot assume returned ids
+        # from supabase insert query will be in the same order that the list of dictionaries of schools is when we pass it in  
+        for school in schools: # ingest schools
+            school_id = str(uuid.uuid4())
+            schools_as_dict.append({
+                "id": school_id,
+                "name":school.name,
+                "country_code": school.country_code
+            })
+
+            school_id_to_domains[school_id] = school.domains
+
         _ = (
-            handler.supabase_client.table("schools").upsert(schools_as_dict).execute()
+            handler.supabase_client.table("schools").insert(schools_as_dict).execute()
+        )
+
+        # ingest school domains
+        all_schools_and_domains = []
+        for school_id in school_id_to_domains:
+            
+            all_domains = school_id_to_domains[school_id]
+            for domain in all_domains:
+                all_schools_and_domains.append({
+                "school_id": school_id,
+                "domain": domain
+            })
+        
+        _ = (
+            handler.supabase_client.table("school_email_domains").insert(all_schools_and_domains).execute()
         )
 
 if __name__ == "__main__":
@@ -59,11 +88,5 @@ if __name__ == "__main__":
     ingestor = SchoolsIngestor(handler, "./world_universities_and_domains.json")    
 
     schools_list = ingestor.parse_schools_list()
-    school_name_set = set()
-    for school in schools_list:
-        if school.name in school_name_set:
-            print(school.name)
-        else:
-            school_name_set.add(school.name)
 
     ingestor.ingest_schools_into_supabase(schools_list)
