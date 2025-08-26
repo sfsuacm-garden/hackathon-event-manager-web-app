4d0d76da05bab127d35057054e2a09485a8cee59
AkimT13 <akimtarasov@gmail.com>
2025-08-25 19:46:49 -0700

    Connected backend api to Supabase Via Prisma.

---
+++ app/api/src/config/postgres.ts
@@ -0,0 +1,7 @@
+// For direct interaction with database
+
+import postgres from 'postgres';
+const connectionString: string = process.env.DATABASE_URL ?? 'Env var not found';
+const sql:postgres.Sql<{}> = postgres(connectionString);
+
+export default sql;
