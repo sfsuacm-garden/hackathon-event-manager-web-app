92de45f7b77222e01c0a5923aad533742023f642
Odera Nwosu <55517700+oderanwosu@users.noreply.github.com>
2025-09-06 13:18:09 -0700

    Delete app/api/src/zod/schemas directory

--- app/api/src/zod/schemas/objects/NullableBoolFieldUpdateOperationsInput.schema.ts
+++
@@ -1,9 +0,0 @@
-import { z } from 'zod';
-import type { Prisma } from '@prisma/client';
-
-
-const makeSchema = (): z.ZodObject<any> => z.object({
-  set: z.boolean().nullish()
-}).strict();
-export const NullableBoolFieldUpdateOperationsInputObjectSchema: z.ZodType<Prisma.NullableBoolFieldUpdateOperationsInput> = makeSchema() as unknown as z.ZodType<Prisma.NullableBoolFieldUpdateOperationsInput>;
-export const NullableBoolFieldUpdateOperationsInputObjectZodSchema = makeSchema();
--- app/api/src/zod/schemas/objects/NullableStringFieldUpdateOperationsInput.schema.ts
+++
@@ -1,9 +0,0 @@
-import { z } from 'zod';
-import type { Prisma } from '@prisma/client';
-
-
-const makeSchema = (): z.ZodObject<any> => z.object({
-  set: z.string().nullish()
-}).strict();
-export const NullableStringFieldUpdateOperationsInputObjectSchema: z.ZodType<Prisma.NullableStringFieldUpdateOperationsInput> = makeSchema() as unknown as z.ZodType<Prisma.NullableStringFieldUpdateOperationsInput>;
-export const NullableStringFieldUpdateOperationsInputObjectZodSchema = makeSchema();
