ba6360b9a910c3e29ef6d1959dd79adb1c83f7e0
Odera Nwosu <55517700+oderanwosu@users.noreply.github.com>
2025-09-10 18:30:52 -0700

    enhance: added loading state to join team page

--- app/applicant-portal/src/app/join/page.tsx
+++ app/applicant-portal/src/app/join/page.tsx
@@ -16,6 +16,7 @@ import {
   AlertTitle,
 } from "@/components/shadcn/ui/alert";
 import { Icons } from "@/lib/icons";
+import { Spinner } from "@/components/ui/shadcn-io/spinner";
 
 export default function JoinPage() {
   var isTeamManagementUnlocked = true;
@@ -23,6 +24,7 @@ export default function JoinPage() {
   var isTeamValid = true;
   var isTeamFull = true;
   var teamName = "TeamName01";
+  var isLoading = true;
 
   const member = {
     name: "John Smith",
@@ -32,88 +34,97 @@ export default function JoinPage() {
       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSe-UxhlswN7mS_tqXj0L9a00hL_NHisSSqn4Mm6FW8BuvQI_8wHrLG7pUlkvozlpwASp0&usqp=CAU",
     initials: 200,
   };
+
   return (
     <main className="min-h-screen flex items-center justify-center p-4">
       <Card className="max-w-md w-full">
-        {!isTeamFull ? (
-          <CardHeader>
-            <CardTitle>
-              <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">
-                You are joining {teamName}
-              </h3>
-            </CardTitle>
-            <CardDescription>
-              This means that your application to participate in SF Hacks 2026
-              will be evualated together.
-            </CardDescription>
-          </CardHeader>
+        {isLoading ? (
+          <div className="flex items-center justify-center h-50">
+            <Spinner />
+          </div>
         ) : (
-          <CardHeader>
-            <CardTitle>
-              <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">
-                {teamName} is unable to accept new members.
-              </h3>
-            </CardTitle>
-            <CardDescription>
-              This team has hit max capacity and cannot accept anymore team
-              members. Try joining another team.
-            </CardDescription>
-          </CardHeader>
-        )}
+          <>
+            {!isTeamFull ? (
+              <CardHeader>
+                <CardTitle>
+                  <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">
+                    You are joining {teamName}
+                  </h3>
+                </CardTitle>
+                <CardDescription>
+                  This means that your application to participate in SF Hacks
+                  2026 will be evualated together.
+                </CardDescription>
+              </CardHeader>
+            ) : (
+              <CardHeader>
+                <CardTitle>
+                  <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">
+                    {teamName} is unable to accept new members.
+                  </h3>
+                </CardTitle>
+                <CardDescription>
+                  This team has hit max capacity and cannot accept anymore team
+                  members. Try joining another team.
+                </CardDescription>
+              </CardHeader>
+            )}
 
-        <CardContent className="flex flex-col gap-4">
-          {isUserOnTeam && (
-            <Alert variant="default">
-              <Icons.alert className="text-accent" />
-              <AlertDescription className=" text-accent">
-                <p>
-                  It looks like you are currently already in a team. Joining{" "}
-                  <b>{teamName}</b> will
-                  <span>
-                    {" "}
-                    <b>remove</b>{" "}
-                  </span>
-                  you from your previous team.
-                </p>
-              </AlertDescription>
-            </Alert>
-          )}
-          {!isTeamFull && (
-            <Card>
-              <CardContent>
-                <div className="flex gap-x-2 items-baseline">
-                  <small className="text-sm leading-none font-semibold">
-                    {teamName} Team
-                  </small>
-                  <code className="bg-muted relative rounded px-[0.3rem] py-[0.2rem] font-mono text-sm font-semibold">
-                    {1}/{4}
-                  </code>
+            <CardContent className="flex flex-col gap-4">
+              {isUserOnTeam && (
+                <Alert variant="default">
+                  <Icons.alert className="text-accent" />
+                  <AlertDescription className=" text-accent">
+                    <p>
+                      It looks like you are currently already in a team. Joining{" "}
+                      <b>{teamName}</b> will
+                      <span>
+                        {" "}
+                        <b>remove</b>{" "}
+                      </span>
+                      you from your previous team.
+                    </p>
+                  </AlertDescription>
+                </Alert>
+              )}
+              {!isTeamFull && (
+                <Card>
+                  <CardContent>
+                    <div className="flex gap-x-2 items-baseline">
+                      <small className="text-sm leading-none font-semibold">
+                        {teamName} Team
+                      </small>
+                      <code className="bg-muted relative rounded px-[0.3rem] py-[0.2rem] font-mono text-sm font-semibold">
+                        {1}/{4}
+                      </code>
+                    </div>
+                    {[...Array(2)].map((_, idx) => (
+                      <>
+                        <MemberPreview key={idx} />
+                        <Separator />
+                      </>
+                    ))}
+                  </CardContent>
+                </Card>
+              )}
+              {!isTeamFull ? (
+                <div className="space-x-2">
+                  <Button onClick={() => alert("Hello from shadcn!")}>
+                    Join New Team
+                  </Button>
+                  <Button
+                    variant={"secondary"}
+                    onClick={() => alert("Hello from shadcn!")}
+                  >
+                    Decline
+                  </Button>
                 </div>
-                {[...Array(2)].map((_, idx) => (
-                  <>
-                    <MemberPreview key={idx} />
-                    <Separator />
-                  </>
-                ))}
-              </CardContent>
-            </Card>
-          )}
-          {!isTeamFull ? (
-            <div className="space-x-2">
-              <Button onClick={() => alert("Hello from shadcn!")}>
-                Join New Team
-              </Button>
-              <Button
-                variant={"secondary"}
-                onClick={() => alert("Hello from shadcn!")}
-              >
-                Decline
-              </Button>
-            </div>
-          ) : (
-            <Button variant="outline">Back to Dashboard</Button>
-          )}
-        </CardContent>
+              ) : (
+                <Button variant="outline">Back to Dashboard</Button>
+              )}
+            </CardContent>
+          </>
+        )}
       </Card>
     </main>
   );
