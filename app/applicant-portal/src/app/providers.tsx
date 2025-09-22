e58e62e7ddf95676e6ee0b2cd8693e4f72a0f8d3
AkimT13 <akimtarasov@gmail.com>
2025-09-22 10:13:32 -0700

    attempting to reslove merge conflicts with dev branch

--- app/applicant-portal/src/app/providers.tsx
+++ app/applicant-portal/src/app/providers.tsx
@@ -1,10 +1,30 @@
-'use client'
-import {QueryClient, QueryClientProvider} from '@tanstack/react-query'
+"use client";
 
-import { ReactNode, useState } from 'react';
+import { trpc } from "@/utils/trpc";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { httpBatchLink } from "@trpc/client";
+import { useState } from "react";
 
-export default function Providers({children}: {children:ReactNode}){
-    const [queryClient] = useState(()=> new QueryClient());
-    return <QueryClientProvider client ={queryClient}>{children}</QueryClientProvider>
+export default function Providers({ children }: { children: React.ReactNode }) {
+  const [queryClient] = useState(() => new QueryClient());
+  const [trpcClient] = useState(() =>
+    trpc.createClient({
+      links: [
+        httpBatchLink({
+          url: process.env.PUBLIC_API_URL + "/trpc",
+          headers() {
+            return {
+              // Add auth headers if needed
+            };
+          },
+        }),
+      ],
+    })
+  );
 
+  return (
+    <trpc.Provider client={trpcClient} queryClient={queryClient}>
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    </trpc.Provider>
+  );
 }
