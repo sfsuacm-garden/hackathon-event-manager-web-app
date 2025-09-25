09472937bf827b7552aac87859ba6144e2a9646a
Mikko <realmiks27@gmail.com>
2025-09-25 16:28:07 -0700

    reinstalled de pendencies

--- app/applicant-portal/src/utils/trpc.ts
+++ app/applicant-portal/src/utils/trpc.ts
@@ -1,5 +1,5 @@
 import { createTRPCReact } from '@trpc/react-query';
-import type { AppRouter } from '../../../api/src/core/_app';
+import type {AppRouter } from '../../../api/src/core/_app';
 
 /** 
  *  The critical entry point for connection between the backedn and frontend. This should be seperated from the rest of the app logic to avoid calls outside of the client. 
