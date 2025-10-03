55b02bef5ba97c04058e64b06d37fad29ce0bbbc
DiscoDoggy <akaattarilim@gmail.com>
2025-10-03 00:46:36 -0700

    add teams to app.ts for trpc routing

--- app/api/src/core/_app.ts
+++ app/api/src/core/_app.ts
@@ -1,9 +1,11 @@
 import { eventRouter } from '../features/event/event.routes';
+import { teamsRouter } from '../features/teams/teams.routes';
 import { t } from './trpc';
 
 export const trpcRouter = t.router({
   //TODO change event directory to be plural.
-  events: eventRouter
+  events: eventRouter,
+  teams: teamsRouter
 });
 
 export type AppRouter = typeof trpcRouter;
