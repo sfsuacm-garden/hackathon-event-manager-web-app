7874c2c2d866bdbff24dd2882bb52de76bb25184
DiscoDoggy <akaattarilim@gmail.com>
2025-10-09 20:06:54 -0700

    Make join path parameter async

--- app/applicant-portal/src/app/join/[teamId]/page.tsx
+++ app/applicant-portal/src/app/join/[teamId]/page.tsx
@@ -1,6 +1,6 @@
 import JoinPage from "./JoinPage";
 
-export default function Page({ params }: { params: { teamId: string } }) {
-  const { teamId } = params;
+export default async function Page({ params }: { params: Promise<{ teamId: string }> }) {
+  const { teamId } = await params;
   return <JoinPage teamIdToJoin={teamId} />;
 }
