240b839b4e58525662b66f634c8bc2b484534273
Odera Nwosu <55517700+oderanwosu@users.noreply.github.com>
2025-10-20 09:48:45 -0700

    Fix linting error with unwaited search params

--- app/applicant-portal/src/app/(auth)/verify-otp/page.tsx
+++ app/applicant-portal/src/app/(auth)/verify-otp/page.tsx
@@ -1,137 +1,18 @@
 "use client";
 
-import {
-  Alert,
-  AlertDescription,
-  AlertTitle,
-} from "@/components/shadcn/ui/alert";
-import { Button } from "@/components/shadcn/ui/button";
-import {
-  Field,
-  FieldDescription,
-  FieldError,
-  FieldGroup,
-  FieldLabel,
-} from "@/components/shadcn/ui/field";
-import {
-  InputOTP,
-  InputOTPGroup,
-  InputOTPSlot,
-} from "@/components/shadcn/ui/input-otp";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import { Spinner } from "@/components/ui/shadcn-io/spinner";
-import { useSearchParams } from "next/navigation";
-import { useMemo } from "react";
-import { useOtpVerification } from "./hooks";
+import { VerifyOtpPage } from "./components/VerifyOTPPage";
 
-export default function AuthBase() {
-  const searchParams = useSearchParams();
-  const email = useMemo(() => searchParams.get("email") ?? "", [searchParams]);
-  const authFlow = useMemo(
-    () => searchParams.get("auth") as "signup" | "login" | null,
-    [searchParams]
-  );
+type VerifyOtpPageProps = {
+  searchParams: Promise<{
+    email?: string;
+    auth?: "signup" | "login";
+  }>;
+};
 
-  const {
-    otp,
-    setOtp,
-    handleResend,
-    isVerifyOtpPending,
-    isVerifyOtpError,
-    verifyOtpError,
-    isresendOtpPending,
-    handleVerify,
-    onRenavigate,
-    messages,
-  } = useOtpVerification(email, authFlow);
+export default async function VerifyOtp({ searchParams }: VerifyOtpPageProps) {
+  const params = await searchParams;
+  const email = params?.email ?? "";
+  const authFlow = params?.auth ?? null;
 
-  return (
-    <main className="flex justify-center items-center  p-4">
-      <Card className="w-full sm:max-w-md">
-        <CardHeader>
-          <CardTitle>Verify Account</CardTitle>
-          <CardDescription>{messages.description}</CardDescription>
-        </CardHeader>
-
-        <CardContent>
-          <form onSubmit={handleVerify} className="space-y-6">
-            <FieldGroup>
-              <Field>
-                <FieldLabel>One-Time Password</FieldLabel>
-                <InputOTP
-                  maxLength={6}
-                  value={otp}
-                  onChange={(val: string) =>
-                    setOtp(val.replace(/\D/g, "").slice(0, 6))
-                  }
-                >
-                  <InputOTPGroup>
-                    {Array.from({ length: 6 }).map((_, idx) => (
-                      <InputOTPSlot key={idx} index={idx} />
-                    ))}
-                  </InputOTPGroup>
-                </InputOTP>
-                <FieldDescription>
-                  Please enter the one-time password sent to your email or
-                  phone.
-                </FieldDescription>
-                {isVerifyOtpError && (
-                  <FieldError errors={[{ message: verifyOtpError?.message ?? ""}]} />
-                )}
-              </Field>
-            </FieldGroup>
-
-            <Button
-              type="submit"
-              className="w-full"
-              disabled={isVerifyOtpPending}
-            >
-              {isVerifyOtpPending ? (
-                <span className="flex items-center justify-center gap-2">
-                  <Spinner className="h-4 w-4 animate-spin" />
-                  Verifying...
-                </span>
-              ) : (
-                messages.buttonText
-              )}
-            </Button>
-
-            <CardFooter className="flex justify-between text-xs text-muted-foreground">
-              <button
-                type="button"
-                onClick={onRenavigate}
-                className="underline underline-offset-2"
-                disabled={isVerifyOtpPending}
-              >
-                {messages.renavigateText}
-              </button>
-
-              <button
-                type="button"
-                onClick={handleResend}
-                className="underline underline-offset-2"
-                disabled={isVerifyOtpPending || isresendOtpPending}
-              >
-                {messages.resendText}
-              </button>
-            </CardFooter>
-
-            {isVerifyOtpError && (
-              <Alert variant="destructive" className="mt-2">
-                <AlertTitle>Error</AlertTitle>
-                <AlertDescription>{verifyOtpError?.message}</AlertDescription>
-              </Alert>
-            )}
-          </form>
-        </CardContent>
-      </Card>
-    </main>
-  );
+  return <VerifyOtpPage email={email} authFlow={authFlow} />;
 }
