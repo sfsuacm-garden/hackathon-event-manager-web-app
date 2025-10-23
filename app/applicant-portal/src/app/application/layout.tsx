d76240aafb1094eff9e2158d2e0432627bcedc71
Odera Nwosu <55517700+oderanwosu@users.noreply.github.com>
2025-10-22 21:50:12 -0700

    fix spacing on application page layout

--- app/applicant-portal/src/app/application/layout.tsx
+++ app/applicant-portal/src/app/application/layout.tsx
@@ -8,7 +8,9 @@ export default function RootLayout({
   return (
     <AuthOnlyProvider>
       <BaseProtectedProvider>
-       {children}
+         <main className="mx-auto w-full max-w-lg px-6 py-12 space-y-8 ">
+        {children}
+      </main>
       </BaseProtectedProvider>
     </AuthOnlyProvider>
   );
