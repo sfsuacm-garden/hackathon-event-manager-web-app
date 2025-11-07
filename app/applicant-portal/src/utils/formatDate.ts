74248e0eb6b1a915f02f8b049044a1ffae21ad2a
Aljhay Soriano <aljhaysoriano@Aljhays-MacBook-Pro.local>
2025-11-07 13:23:36 -0800

    test

--- app/applicant-portal/src/utils/formatDate.ts
+++ app/applicant-portal/src/utils/formatDate.ts
@@ -1,15 +1,19 @@
+import humanizeDuration from "humanize-duration";
+
 export function formatDate(startDate: string): string | null {
-    const date = new Date(startDate);
-    return date.toLocaleString("en-US", {year: "numeric", month: "long", day: "numeric", hour: "numeric", minute: "2-digit"});
+    const date = new Date(startDate).getTime(); 
+    return humanizeDuration(date, { round: true, units: ["d", "m", "y"]})
 }
 
 export function formatDateDifference(startDate: string): string | null {
-    const start = new Date(startDate);
-    const current = new Date();
+    const start = new Date(startDate).getTime();
+    const current = new Date().getTime();
 
-    const difference = current.getDay() - start.getDay();
+    const difference = current - start;
+   
+    const humanizedTime = humanizeDuration(difference, { round: true, units: ["d"]});
 
-    return String(difference) + " days ago";
+    return humanizedTime + " ago";
 }
 
 export default {formatDate, formatDateDifference};
