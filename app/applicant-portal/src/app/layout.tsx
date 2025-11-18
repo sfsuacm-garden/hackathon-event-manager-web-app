3376413dd8d72e4dfe143334025ea41c5c2e57d2
Mikko <realmiks27@gmail.com>
2025-11-18 11:22:14 -0800

    add base url to show openGraph image

--- app/applicant-portal/src/app/layout.tsx
+++ app/applicant-portal/src/app/layout.tsx
@@ -16,11 +16,13 @@ const geistMono = Geist_Mono({
   subsets: ['latin']
 });
 
+const baseUrl = process.env.NODE_ENV === 'production' ? 'https://app.sfhacks.io/' : '';
+
 export const metadata: Metadata = {
   title: 'SF Hacks 2026 Application Signup',
   description:
     'Sign up to participate in SF Hacks 2026, the premier student hackathon in San Francisco. Submit your application to join the event and showcase your skills!',
-  icons: 'logo_pink.png',
+  icons: `${baseUrl}/logo_pink.png`,
   keywords: [
     'SF Hacks 2026',
     'hackathon',
@@ -30,6 +32,7 @@ export const metadata: Metadata = {
     'sf hacks'
   ],
   authors: [{ name: 'SFHacks Team', url: 'https://sfhacks.io' }],
+  metadataBase: new URL(baseUrl),
   openGraph: {
     title: 'SF Hacks 2026 Application Signup',
     description:
@@ -37,14 +40,15 @@ export const metadata: Metadata = {
     siteName: 'Apply to SF Hacks 2026',
     images: [
       {
-        url: '/banner1.png',
+        url: `${baseUrl}/banner1.png`,
         width: 1200,
         height: 630,
         alt: 'SFHacks Hackathon'
       }
     ],
     locale: 'en_US',
-    type: 'website'
+    type: 'website',
+    url: baseUrl,
   }
 };
 
