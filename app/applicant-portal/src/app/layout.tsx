f3054e1437794950d1621f9dc755b06fe38e3000
Mikko <realmiks27@gmail.com>
2025-11-20 10:54:34 -0800

    update baseUrl to not hardcode links

--- app/applicant-portal/src/app/layout.tsx
+++ app/applicant-portal/src/app/layout.tsx
@@ -16,7 +16,7 @@ const geistMono = Geist_Mono({
   subsets: ['latin']
 });
 
-const baseUrl = process.env.NODE_ENV === 'production' ? 'https://app.sfhacks.io/' : 'http://localhost:3000';
+const baseUrl = process.env.NODE_ENV === 'production' ? process.env.NEXT_PUBLIC_PROD_URL! : process.env.NEXT_PUBLIC_DEV_URL!;
 
 export const metadata: Metadata = {
   title: 'SF Hacks 2026 Application Signup',
