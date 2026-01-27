8cea666bffc446fb0e012f2ae5e89801c3b3b270
Odera Nwosu <55517700+oderanwosu@users.noreply.github.com>
2026-01-26 19:13:45 -0800

    fix: remove property assignment from event API

--- app/api/src/features/event/event.routes.ts
+++ app/api/src/features/event/event.routes.ts
@@ -20,7 +20,6 @@ export const eventRouter = t.router({
   // TODO
   me:  t.procedure.use(requireEvent).query(async ({ctx}) => {
     const event =  await getEventById(ctx.event.id);
-    event.isTeamManagementOpen = false
     return event
   }),
 });
