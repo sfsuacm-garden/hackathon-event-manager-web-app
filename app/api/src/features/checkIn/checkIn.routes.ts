6dfd4d4f8e45ec1fb77c41f364005939d853013a
AkimT13 <akimtarasov@gmail.com>
2026-02-12 21:59:48 -0800

    remove requireEventAccess

--- app/api/src/features/checkIn/checkIn.routes.ts
+++ app/api/src/features/checkIn/checkIn.routes.ts
@@ -1,4 +1,4 @@
-import { requireAuth, requireEvent, requireEventAccess } from '../../common/common.middleware';
+import { requireAuth, requireEvent} from '../../common/common.middleware';
 import { t } from '../../core/trpc';
 import { checkInSchema, checkInLookupSchema } from './checkIn.schemas';
 import { checkInUserById, lookupCheckInByUserId } from './checkIn.controller';
@@ -7,7 +7,6 @@ export const checkInRouter = t.router({
   lookup: t.procedure
     .use(requireAuth)
     .use(requireEvent)
-    .use(requireEventAccess)
     .input(checkInLookupSchema)
     .query(({ ctx, input }) =>
       lookupCheckInByUserId(ctx.event.id, input.userId)
@@ -16,7 +15,6 @@ export const checkInRouter = t.router({
   checkIn: t.procedure
     .use(requireAuth)
     .use(requireEvent)
-    .use(requireEventAccess)
     .input(checkInSchema)
     .mutation(({ ctx, input }) =>
       checkInUserById(ctx.event.id, ctx.user?.id || '', input.userId)
